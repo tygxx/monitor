1f71cf520d3f755a10c842ca462c6036dfa4a4c3
tygxx <251380405@qq.com>
2021-01-29 19:33:08 +0800

    first commit

---
+++ src/main/java/com/yy/monitor/MonitorApplication.java
@@ -0,0 +1,18 @@
+package com.yy.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
